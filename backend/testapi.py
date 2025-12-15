ca6425eb25eb0dcbcd1a24c1a4208a580c6004e8
Yaren <yarencnk03@gmail.com>
2025-12-15 13:29:12 +0300

    change port after deploying

--- backend/testapi.py
+++ backend/testapi.py
@@ -137,4 +137,5 @@ def predict():
         return jsonify({"error": f"An unexpected error occurred during prediction: {str(e)}", "type": type(e).__name__}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
